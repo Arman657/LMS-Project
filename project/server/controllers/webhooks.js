60c900582cc6875efee2fb392d5687383f295ed4
webOps-aman <amanhxe@gmail.com>
2025-03-08 05:53:38 +0530

    updated

--- project/server/controllers/webhooks.js
+++ project/server/controllers/webhooks.js
@@ -19,7 +19,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                 const userData = {
                     _id: data.id,
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + " " + data.last_name,
                     imageUrl: data.image_url,
                 }
@@ -55,3 +55,4 @@ export const clerkWebhooks = async (req, res) => {
     }
 
 }
+
