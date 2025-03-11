2af9b6d4d835ed34e48b787cf80416ec33851f5a
webOps-aman <amanhxe@gmail.com>
2025-03-11 22:35:43 +0530

    working on backend

--- project/server/server.js
+++ project/server/server.js
@@ -6,6 +6,8 @@ import { clerkWebhooks } from './controllers/webhooks.js';
 import educatorRouter from './routes/educatorRoutes.js';
 import { clerkMiddleware } from '@clerk/express';
 import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 
 
@@ -28,6 +30,9 @@ app.use(clerkMiddleware());
 app.get('/', (req, res) => res.send('api working'))
 app.post('/clerk', express.json(), clerkWebhooks)
 app.use('/api/educator', express.json(), educatorRouter)
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+
 
 
 
